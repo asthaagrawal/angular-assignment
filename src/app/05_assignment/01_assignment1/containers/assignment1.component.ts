cd1dc9f5c27b395829ee70bd1f259f72df386787
Astha Agrawal <astha.agrawal@nagarro.com>
2020-04-22 05:25:31 +0530

    assignment 01

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './assignment1.component.html',
   styleUrls: ['./assignment1.component.css']
 })
+
+//creating input of type "text"
+
 export class Assignment1Component {
+
+  showInput : string;
+  onInput(showInput: string) {
+    this.showInput = showInput;
+  }
 
 }
